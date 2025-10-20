6c74adc882660a22edbb09b0bfb97aedadeea49d
Fish <15898571400@163.com>
2025-10-20 12:05:06 +0800

    [feat][chat]: 修改滑动条的高度为问题

--- lib/ui/views/TIMUIKitChat/TIMUIKitTextField/tim_uikit_text_field_layout/narrow.dart
+++ lib/ui/views/TIMUIKitChat/TIMUIKitTextField/tim_uikit_text_field_layout/narrow.dart
@@ -541,6 +541,7 @@ class _TIMUIKitTextFieldLayoutNarrowState extends TIMUIKitState<TIMUIKitTextFiel
                                       thumbColor: const Color(0xF8B8B8B8),
                                       trackColor: const Color(0xF8EAEAEA),
                                       trackBorderColor: Colors.transparent,
+                                      padding: EdgeInsets.symmetric(vertical: 4),
                                       child: KeyboardVisibility(
                                           child: ExtendedTextField(
                                               maxLines: 5,
